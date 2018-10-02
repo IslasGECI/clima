ed764bb827a28d87712b6355d546e87f22563045
Efren Cabrera <efren507.507@gmail.com>
2018-10-01 18:21:18 -0700

    Agrega primer versión del reporte de clima

---
+++ docs/js/main.js
@@ -0,0 +1,85 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+var title = document.getElementById("title-sticky");
+var img1 = document.getElementById("img-1");
+var img2 = document.getElementById("img-2");
+var img3 = document.getElementById("img-3");
+var vientoimg1 = document.getElementById("viento-img-1");
+var vientoimg2 = document.getElementById("viento-img-2");
+var vientoimg3 = document.getElementById("viento-img-3");
+var logo = document.getElementById("logo");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    title.classList.add("sticky-title");
+    title.classList.remove("no-sticky-title");
+    logo.classList.add("sticky-logo");
+    logo.classList.remove("no-sticky-logo");
+  } else {
+    title.classList.add("no-sticky-title");
+    title.classList.remove("sticky-title");
+    logo.classList.add("no-sticky-logo");
+    logo.classList.remove("sticky-logo");
+  }
+}
+
+function show24() {
+  img1.classList.add("visible");
+  img1.classList.remove("invisible");
+  img2.classList.add("invisible");
+  img2.classList.remove("visible");
+  img3.classList.add("invisible");
+  img3.classList.remove("visible");
+}
+
+function show48() {
+  img1.classList.add("invisible");
+  img1.classList.remove("visible");
+  img2.classList.add("visible");
+  img2.classList.remove("invisible");
+  img3.classList.add("invisible");
+  img3.classList.remove("visible");
+}
+
+function show72() {
+  img1.classList.add("invisible");
+  img1.classList.remove("visible");
+  img2.classList.add("invisible");
+  img2.classList.remove("visible");
+  img3.classList.add("visible");
+  img3.classList.remove("invisible");
+}
+
+function showwind24() {
+  vientoimg1.classList.add("visible");
+  vientoimg1.classList.remove("invisible");
+  vientoimg2.classList.add("invisible");
+  vientoimg2.classList.remove("visible");
+  vientoimg3.classList.add("invisible");
+  vientoimg3.classList.remove("visible");
+}
+
+function showwind48() {
+  vientoimg1.classList.add("invisible");
+  vientoimg1.classList.remove("visible");
+  vientoimg2.classList.add("visible");
+  vientoimg2.classList.remove("invisible");
+  vientoimg3.classList.add("invisible");
+  vientoimg3.classList.remove("visible");
+}
+
+function showwind72() {
+  vientoimg1.classList.add("invisible");
+  vientoimg1.classList.remove("visible");
+  vientoimg2.classList.add("invisible");
+  vientoimg2.classList.remove("visible");
+  vientoimg3.classList.add("visible");
+  vientoimg3.classList.remove("invisible");
+}
